d5cdd7faaa2e43dd40c42cc3ff383f59668f0a3d
Azharie Muhammad <azhariemuhammad@MacBook-Air-Azharie.local>
2023-02-26 12:42:00 +0800

    feat(boilerplate): change import statements

--- generate-app.js
+++ generate-app.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const { execSync } = require("child_process");
-const path = require("path");
-const fs = require("fs");
+import { execSync } from "child_process";
+import path from "path";
+import fs = from "fs";
 
 if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
