6bca20dda19b6cf52c0858010ce1186990856757
Azharie Muhammad <azhariemuhammad@MacBook-Air-Azharie.local>
2023-02-26 12:32:08 +0800

    feat(boilerplate): add generate-app script

---
+++ generate-app.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+if (process.argv.length < 3) {
+  console.log("You have to provide a name to your app.");
+  console.log("For example :");
+  console.log("    npx create-my-boilerplate my-app");
+  process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const git_repo = "https://github.com/azhariemuhammad/create-vite-netlify";
+
+try {
+  fs.mkdirSync(projectPath);
+} catch (err) {
+  if (err.code === "EXIST") {
+    console.log(
+      `The file ${projectName} already exist in the current directory, please give it another name.`
+    );
+  } else {
+    console.log(error);
+  }
+  process.exit(1);
+}
+
+async function main() {
+  try {
+    console.log("Downloading files...");
+    execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+
+    process.chdir(projectPath);
+
+    console.log("Installing dependencies...");
+    execSync("pnpm install");
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
